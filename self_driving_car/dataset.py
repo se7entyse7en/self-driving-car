0ed04bcbafc13cb13a3c5d8c99ca252e3ef5b6a6
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2018-02-15 00:34:53 +0100

    Modified DatasetGenerator in order to use multiple augmenters together

--- self_driving_car/dataset.py
+++ self_driving_car/dataset.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import cv2
 import pandas as pd
@@ -7,7 +8,8 @@ from self_driving_car.augmentation import HorizontalFlipImageDataAugmenter
 
 class DatasetGenerator(object):
 
-    def __init__(self, csv_path, image_data_augmenters):
+    def __init__(self, csv_path, image_data_augmenters,
+                 data_augmenters_probs=None):
         self._csv_path = csv_path
         self._base_dataset = pd.read_csv(
             self._csv_path, header=None,
@@ -15,12 +17,15 @@ class DatasetGenerator(object):
                    'steering_angle', 'speed', 'throttle', 'brake')
         )
         self._augmenters = image_data_augmenters
+        self._augmenters_probs = (data_augmenters_probs or
+                                  [0.5] * len(self._augmenters))
 
-    def flow(self):
+    def flow(self, use_augmenters=True):
         for _, row in self._base_dataset.iterrows():
-            yield from self._flow_from_row(row)
+            yield from self._flow_from_row(row, use_augmenters)
 
     def preprocess_images(self, images_paths):
+        # Only use center image for now
         yield preprocess_image(images_paths[0])
 
     def save(self, csv_output_path):
@@ -29,7 +34,7 @@ class DatasetGenerator(object):
         os.makedirs(img_dir)
         for i, (_, row) in enumerate(self._base_dataset.iterrows()):
             for j, (image, steering_angle) in enumerate(
-                    self._flow_from_row(row)):
+                    self._flow_from_row(row, True)):
                 image_path = os.path.join(img_dir, f'{i}-{j}.jpg')
                 cv2.imwrite(image_path, cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
                 out_rows.append({
@@ -40,20 +45,23 @@ class DatasetGenerator(object):
         df = pd.DataFrame.from_records(out_rows)
         df.to_csv(csv_output_path)
 
-    def _flow_from_row(self, row):
+    def _flow_from_row(self, row, use_augmenters):
         images_paths = (row['center_image_path'],
                         row['left_image_path'],
                         row['right_image_path'])
         steering_angle = row['steering_angle']
 
         for image in self.preprocess_images(images_paths):
-            # Yield original image (only preprocessed) and steering angle
-            yield image, steering_angle
+            if use_augmenters:
+                for aug, aug_prob in zip(self._augmenters,
+                                         self._augmenters_probs):
+                    if random.random() > aug_prob:
+                        continue
 
-            for aug in self._augmenters:
-                # Yield augmented image for each augmenter and corresponding
-                # steering angle
-                yield self._augment(aug, image, steering_angle)
+                    image, steering_angle = self._augment(
+                        aug, image, steering_angle)
+
+            yield image, steering_angle
 
     def _augment(self, augmenter, image, steering_angle):
         augmented_image = augmenter.process_random(image)
@@ -67,4 +75,4 @@ def preprocess_image(image_path):
     image = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
     # Crop 25 pixels from bottom to remove car parts
     cropped_image = image[:-25, :]
-    return cv2.resize(cropped_image, (64, 64), interpolation=cv2.INTER_AREA)
+    return cv2.resize(cropped_image, (200, 66), interpolation=cv2.INTER_AREA)
