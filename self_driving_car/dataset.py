b297600754dc5810d230de32b2c1cd68591627f0
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2018-02-19 10:25:07 +0100

    Refactored DatasetGenerator and provided methods for generating training and test set

--- self_driving_car/dataset.py
+++ self_driving_car/dataset.py
@@ -1,4 +1,3 @@
-import os
 import random
 
 import cv2
@@ -6,44 +5,58 @@ import pandas as pd
 from self_driving_car.augmentation import HorizontalFlipImageDataAugmenter
 
 
+IMAGE_WIDTH, IMAGE_HEIGHT = 200, 66
+
+
 class DatasetGenerator(object):
 
     def __init__(self, csv_path, image_data_augmenters,
-                 data_augmenters_probs=None):
+                 data_augmenters_probs=None, test_size=0.25):
         self._csv_path = csv_path
-        self._base_dataset = pd.read_csv(
+        self._dataset = pd.read_csv(
             self._csv_path, header=None,
             names=('center_image_path', 'left_image_path', 'right_image_path',
                    'steering_angle', 'speed', 'throttle', 'brake')
         )
+        shuffled_dataset = self._shuffle_dataset(self._dataset)
+
+        n_rows = shuffled_dataset.shape[0]
+        self._training_size = int(n_rows * (1 - test_size))
+        self._test_size = n_rows - self._training_size
+
+        self._training_set = shuffled_dataset.head(self._training_size)
+        self._test_set = shuffled_dataset.tail(-self._test_size)
+
         self._augmenters = image_data_augmenters
         self._augmenters_probs = (data_augmenters_probs or
                                   [0.5] * len(self._augmenters))
 
+    @property
+    def training_size(self):
+        return self._training_size
+
+    @property
+    def test_size(self):
+        return self._test_size
+
     def flow(self, use_augmenters=True):
-        for _, row in self._base_dataset.iterrows():
+        for _, row in self._dataset.iterrows():
             yield from self._flow_from_row(row, use_augmenters)
+
+    def training_set_batch_generator(self, batch_size):
+        yield from self._dataset_batch_generator(
+            self._training_set, batch_size, True)
+
+    def test_set_batch_generator(self, batch_size):
+        yield from self._dataset_batch_generator(
+            self._test_set, batch_size, False)
 
     def preprocess_images(self, images_paths):
         # Only use center image for now
         yield preprocess_image(images_paths[0])
 
-    def save(self, csv_output_path):
-        out_rows = []
-        img_dir = os.path.join(os.path.split(csv_output_path)[0], 'IMG')
-        os.makedirs(img_dir)
-        for i, (_, row) in enumerate(self._base_dataset.iterrows()):
-            for j, (image, steering_angle) in enumerate(
-                    self._flow_from_row(row, True)):
-                image_path = os.path.join(img_dir, f'{i}-{j}.jpg')
-                cv2.imwrite(image_path, cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
-                out_rows.append({
-                    'image_path': image_path,
-                    'steering_angle': steering_angle
-                })
-
-        df = pd.DataFrame.from_records(out_rows)
-        df.to_csv(csv_output_path)
+    def _shuffle_dataset(self, dataset):
+        return dataset.sample(frac=1).reset_index(drop=True)
 
     def _flow_from_row(self, row, use_augmenters):
         images_paths = (row['center_image_path'],
@@ -70,9 +83,25 @@ class DatasetGenerator(object):
 
         return augmented_image, steering_angle
 
+    def _dataset_batch_generator(self, dataset, batch_size, use_augmenters):
+        i = 0
+        batch_images, batch_steerings = [], []
+        while True:
+            for _, row in self._shuffle_dataset(dataset).iterrows():
+                for image, steering_angle in self._flow_from_row(
+                        row, use_augmenters):
+                    batch_images.append(image)
+                    batch_steerings.append(steering_angle)
+                    i += 1
+                    if i == batch_size:
+                        yield batch_images, batch_steerings
+                        i = 0
+                        batch_images, batch_steerings = [], []
+
 
 def preprocess_image(image_path):
     image = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
     # Crop 25 pixels from bottom to remove car parts
     cropped_image = image[:-25, :]
-    return cv2.resize(cropped_image, (200, 66), interpolation=cv2.INTER_AREA)
+    return cv2.resize(cropped_image, (IMAGE_WIDTH, IMAGE_HEIGHT),
+                      interpolation=cv2.INTER_AREA)
